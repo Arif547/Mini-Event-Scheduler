0ccdfe4db2b5a38791ba6453ec2e675b73c1eb7a
Mahabubul Alam Arif <mahabubulalamarif@gmail.com>
2025-07-20 02:19:52 +0600

    first commit

---
+++ Client-Side/src/App.jsx
@@ -0,0 +1,18 @@
+import './App.css'
+import AddEventFiled from './component/AddEventFiled'
+import Event from './component/event'
+
+function App() {
+
+
+  return (
+    <div className='bg-gray-50 p-4'>
+      <div className='p-5 max-w-7xl mx-auto space-y-5'>
+        Hello world  
+      </div>
+    </div>
+
+  )
+}
+
+export default App
